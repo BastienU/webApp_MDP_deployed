9a1347194bf2bd00a709820495ad6c16dcb2cd69
BastienU <bastien.u04@gmail.com>
2025-06-02 15:09:31 +0200

    Managing connection and disconnection + a lot of changes on the appBar

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1294541493"
+    serviceWorkerVersion: "3296574805"
   }
 });
